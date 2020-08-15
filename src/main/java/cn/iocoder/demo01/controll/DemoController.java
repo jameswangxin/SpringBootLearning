6829d76cf2e20d64896ff60793c1928dbe978c1f
wang xin <kkcmjames@gmail.com>
2020-08-15 12:55:02 +0800

    SpringBoot Quick Start demo

---
+++ src/main/java/cn/iocoder/demo01/controll/DemoController.java
@@ -0,0 +1,14 @@
+package cn.iocoder.demo01.controll;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+    @GetMapping("/echo")
+    public String echo() {
+        return "echo";
+    }
+}
